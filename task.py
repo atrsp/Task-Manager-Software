722f498a7e68b03e4096d851904272e4b6defdea
Viosolin <viosolin.vin@gmail.com>
2026-09-07 16:45:36 +0200

    add fonction first version

--- task.py
+++ task.py
@@ -1,17 +1,29 @@
+import argparse
+
 
 def read_command_line () :
 
-def read_file (nom_file) :
+def read_file (filename) :
 
-def add_task (description, id) :
-    file_content = read_file(mon_file)
-    if id in file_content.keys():
+def add_task (id_str, description) :
+    file_content = read_file(filename)
+    if id_str in file_content.keys():
         raise ValueError("choosen id is already taken")
-    with open('mon_file','w') as f :
-        f.write(id, description)
+    with open('mon_file','a') as f :
+        f.write(str(id_str)+', '+ description+'\n')
+        f.close()
         return id
 
-def modify_task () :
+def modify_task (id_str, new_description) :
+    file_content = read_file(filename)
+    if id_str not in file_content:
+        return "erreur, id non trouvé"
+    file_content[id_str]=new_description
+    with open('mon_file','w') as f :
+        for elt in file_content :
+            f.write(str(elt)+', '+ file_content[elt] + '\n')
+
+
 
 def rm_task () :
 
