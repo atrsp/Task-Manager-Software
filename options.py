4c2fec09922429b05573d5d83524d55f3c8e7b8d
Thomas Jullière <thomas.julliere@imt-atlantique.net>
2026-09-09 15:17:42 +0200

    ajout de options.py

---
+++ options.py
@@ -0,0 +1,9 @@
+import argparse
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
+    subparsers=parser.add_subparsers(dest='fonction')
+    parser_add=subparsers.add('add')
+    parser_add.add_argument('commentary')
+    return parser
