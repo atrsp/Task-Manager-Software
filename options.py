b5a9be4d0b13fb744848117d6d2191b23dd87193
Thomas Jullière <thomas.julliere@imt-atlantique.net>
2026-09-09 16:12:14 +0200

    options complété

--- options.py
+++ options.py
@@ -4,6 +4,17 @@ def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('filename')
     subparsers=parser.add_subparsers(dest='fonction')
+
     parser_add=subparsers.add('add')
     parser_add.add_argument('commentary')
+
+    parser_add=subparsers.modify('modify')
+    parser_add.add_argument('id')
+    parser_add.add_argument('commentary')
+
+    parser_add=subparsers.rm('rm')
+    parser_add.add_argument('id')
+
+    parser_add=subparsers.show('show')
+
     return parser
