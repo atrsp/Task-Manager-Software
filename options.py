7bd475a3a9b53e2354e96a9ae5526f7c8244095e
Viosolin <viosolin.vin@gmail.com>
2026-09-10 09:36:20 +0200

    modification of options for extension 1

--- options.py
+++ options.py
@@ -6,10 +6,12 @@ def create_parser():
     subparsers=parser.add_subparsers(dest='fonction')
     parser_add=subparsers.add_parser('add')
     parser_add.add_argument('commentary')
+    parser_add.add_argument('state')
 
     parser_modify=subparsers.add_parser('modify')
     parser_modify.add_argument('id',type=int)
     parser_modify.add_argument('commentary')
+    parser_modify.add_argument('state')
 
     parser_remove=subparsers.add_parser('rm')
     parser_remove.add_argument('id',type=int)
