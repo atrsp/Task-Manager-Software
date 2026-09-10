41537fa5c997f9d2cad7ac0389625b01767471ce
Viosolin <viosolin.vin@gmail.com>
2026-09-10 10:10:40 +0200

    working extension 1

--- options.py
+++ options.py
@@ -8,18 +8,18 @@ def create_parser():
     parser_add=subparsers.add_parser('add')
     parser_add.add_argument('state')
     parser_add.add_argument('commentary')
-    parser_add.add_argument('state')
 
     parser_modify=subparsers.add_parser('modify')
     parser_modify.add_argument('id',type=int)
     parser_modify.add_argument('state')
     parser_modify.add_argument('commentary')
-    parser_modify.add_argument('state')
 
     parser_modify_commentary=subparsers.add_parser('modify_commentary')
+    parser_modify_commentary.add_argument('id',type=int)
     parser_modify_commentary.add_argument('commentary')
 
     parser_modify_state=subparsers.add_parser('modify_state')
+    parser_modify_state.add_argument('id',type=int)
     parser_modify_state.add_argument('state')
 
     parser_remove=subparsers.add_parser('rm')
