2330779d9bc6479e10c761627113f3ebe2fe9384
Thomas Jullière <thomas.julliere@imt-atlantique.net>
2026-09-11 09:38:22 +0200

    ajout context

--- options.py
+++ options.py
@@ -7,11 +7,13 @@ def create_parser():
 
     parser_add=subparsers.add_parser('add')
     parser_add.add_argument('status')
+    parser_add.add_argument('context')
     parser_add.add_argument('description')
 
     parser_modify=subparsers.add_parser('modify')
     parser_modify.add_argument('id',type=int)
     parser_modify.add_argument('status')
+    parser_modify.add_argument('context')
     parser_modify.add_argument('description')
 
     parser_modify_description=subparsers.add_parser('modify_description')
@@ -21,6 +23,10 @@ def create_parser():
     parser_modify_status=subparsers.add_parser('modify_status')
     parser_modify_status.add_argument('id',type=int)
     parser_modify_status.add_argument('status')
+
+    parser_modify_context=subparsers.add_parser('modify_context')
+    parser_modify_context.add_argument('id',type=int)
+    parser_modify_context.add_argument('context')
 
     parser_remove=subparsers.add_parser('rm')
     parser_remove.add_argument('id',type=int)
--- task.py
+++ task.py
@@ -6,28 +6,28 @@ def read_file (filename) :
             config = json.load(config_file)
             tasks = {}
             for row in f :
-                id_str, status, description = row.strip().split(",", maxsplit=3)
+                id_str, status, context,description = row.strip().split(",", maxsplit=4)
                 if status not in config['status'] :
                     print(f'{status} is not a status. The task with id {id_str} will be erased.')
                 else:
-                    tasks[int(id_str)] = (status, description)
+                    tasks[int(id_str)] = (status,context, description)
     return tasks
 
-def add_task (tasks, description, status) :
+def add_task (tasks, description, status,context) :
     with open('config.json', 'r') as config_file:
         config = json.load(config_file)
 
         if status in config['status'] :
             id = max(tasks.keys()) + 1 if tasks else 1
-            tasks[id] = (status, description)
-            print(f"Task added. Id: {id}, Status: {status}, Description: {description}")
+            tasks[id] = (status,context, description)
+            print(f"Task added. Id: {id}, Status: {status}, Context :{context}, Description: {description}")
 
         else :
             print(f'{status} is not a status. Use one the following: {config["status"]}')
         
     return tasks
 
-def modify_task (id, tasks, new_description, status) :
+def modify_task (id, tasks, new_description, status,context) :
     with open('config.json', 'r') as config_file:
         config = json.load(config_file)
         
@@ -35,7 +35,7 @@ def modify_task (id, tasks, new_description, status) :
         if status not in config['status'] :
             print(f'{status} is not a status. Use one the following: {config["status"]}')
         else:
-            tasks[id] = (status, new_description)
+            tasks[id] = (status, context,new_description)
             print(f"Task {id} modified.")
     else :
         print("ID not found.")
@@ -60,13 +60,13 @@ def show_task(tasks) :
 
 def save_changes (filename, tasks) :
     with open (filename, "w") as f :
-        for id, (status, description) in tasks.items() :
-            f.write(f"{id},{status},{description}\n")
+        for id, (status,context, description) in tasks.items() :
+            f.write(f"{id},{status},{context},{description}\n")
 
 
 def search_task(tasks, word):
     filtered_tasks = {}
-    for id,(status, description) in tasks.items() :
-        if word in status or word in description :
-            filtered_tasks[id]=(status,description)
+    for id,(status, context, description) in tasks.items() :
+        if word in status or word in description or word in context :
+            filtered_tasks[id]=(status, context,description)
     show_task(filtered_tasks)
